5bd11631e226153677e5c9aba3f7b51479f28d24
David Zoike <divad.ekioz@web.de>
2022-12-02 17:54:28 +0100

    google

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,9 @@ import NewNavbar from "./Navbar";
 import Heading from "./Heading";
 import Footer from "./Footer";
 import Main from "./Main";
+import ReactGA from 'react-ga';
+  const TRACKING_ID = "G-ZQWHQPRQVK"; // OUR_TRACKING_ID
+  ReactGA.initialize(TRACKING_ID);
 
 function App() {
   return (
